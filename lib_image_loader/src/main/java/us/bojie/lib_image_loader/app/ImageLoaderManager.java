fdfc151079fbe5a5c4a880a1641b92d2bbb976c4
Bojie Jiang <jiangbojie@gmail.com>
2020-03-01 22:22:40 -0800

    ImageLoaderManager

---
+++ lib_image_loader/src/main/java/us/bojie/lib_image_loader/app/ImageLoaderManager.java
@@ -0,0 +1,147 @@
+package us.bojie.lib_image_loader.app;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.NotificationTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import us.bojie.lib_image_loader.R;
+import us.bojie.lib_image_loader.image.Utils;
+
+import static com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions.withCrossFade;
+
+public class ImageLoaderManager {
+    private ImageLoaderManager() {
+    }
+
+    private static class SingletonHolder {
+        private static ImageLoaderManager instance = new ImageLoaderManager();
+    }
+
+    public static ImageLoaderManager getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    public void displayImageForView(ImageView imageView, String url) {
+        Glide.with(imageView.getContext())
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .transition(BitmapTransitionOptions.withCrossFade())
+                .into(imageView);
+    }
+
+    public void displayImageForCircle(final ImageView imageView, String url) {
+        Glide.with(imageView.getContext())
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .into(new BitmapImageViewTarget(imageView) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory
+                                .create(imageView.getResources(), resource);
+                        drawable.setCircular(true);
+                        imageView.setImageDrawable(drawable);
+                    }
+                });
+    }
+
+
+    public void displayImageForViewGroup(final ViewGroup group, String url, final boolean needBlur) {
+        Glide.with(group.getContext())
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .into(new SimpleTarget<Bitmap>() {
+                          @SuppressLint("CheckResult")
+                          @Override
+                          public void onResourceReady(@NonNull Bitmap resource,
+                                                      @Nullable Transition<? super Bitmap> transition) {
+                              Observable.just(resource).map(new Function<Bitmap, Drawable>() {
+                                  @Override
+                                  public Drawable apply(Bitmap bitmap) throws Exception {
+                                      if (needBlur) {
+                                          return new BitmapDrawable(group.getResources(),
+                                                  Utils.doBlur(bitmap, 100, true));
+                                      } else {
+                                          return new BitmapDrawable(group.getResources(), bitmap);
+                                      }
+                                  }
+                              }).subscribeOn(Schedulers.io())
+                                      .observeOn(AndroidSchedulers.mainThread())
+                                      .subscribe(new Consumer<Drawable>() {
+                                          @Override
+                                          public void accept(Drawable drawable) throws Exception {
+                                              group.setBackground(drawable);
+                                          }
+                                      });
+                          }
+                      }
+                );
+
+    }
+
+    public void displayImageForNotification(Context context, RemoteViews rv,
+                                            int id, Notification notification,
+                                            int NOTIFICATION_ID, String url) {
+        displayImageForTarget(context,
+                initNotificationTarget(context, rv, id, notification, NOTIFICATION_ID),
+                url);
+    }
+
+    private NotificationTarget initNotificationTarget(Context context, RemoteViews rv,
+                                                      int id, Notification notification,
+                                                      int NOTIFICATION_ID) {
+        return new NotificationTarget(context, id, rv, notification, NOTIFICATION_ID);
+    }
+
+    /**
+     * 为非view加载图片
+     */
+    private void displayImageForTarget(Context context, Target target, String url) {
+        Glide.with(context)
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .transition(withCrossFade())
+                .fitCenter()
+                .into(target);
+    }
+
+    @SuppressLint("CheckResult")
+    private RequestOptions initCommonRequestOption() {
+        RequestOptions options = new RequestOptions();
+        options.placeholder(R.mipmap.b4y)
+                .error(R.mipmap.b4y)
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                .skipMemoryCache(false)
+                .priority(Priority.NORMAL);
+        return options;
+    }
+}
